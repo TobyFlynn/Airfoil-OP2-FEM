44fae895b0b04b93fb541496db2365f9e6477d3f
Toby Flynn <toby_flynn@btinternet.com>
2020-12-16 16:02:56 +0000

    Adding CGNS to CMake and opening CGNS file in program

--- src/airfoil.cpp
+++ src/airfoil.cpp
@@ -1,10 +1,18 @@
 #include "op_seq.h"
 
+#include "cgnslib.h"
+
 int main(int argc, char **argv) {
   // Initialise OP2
   op_init(argc, argv, 2);
 
-  op_printf("Minimal OP2 Program\n");
+  // Read CGNS grid
+  int file;
+  if(cg_open("./naca0012.cgns", CG_MODE_READ, &file)) {
+    cg_error_exit();
+  }
+
+  cg_close(file);
 
   // Clean up OP2
   op_exit();
