c1e39f2b90a463105c0f772a452777e92f6ab1a0
Toby Flynn <toby_flynn@btinternet.com>
2021-01-11 14:58:13 +0000

    Started adding constants (solution points and matrices) and updated how CGNS mesh is loaded

--- src/airfoil.cpp
+++ src/airfoil.cpp
@@ -7,8 +7,28 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <algorithm>
+
+#include "constantsDG.h"
 
 using namespace std;
+
+// Template functions for op_decl_map due to using CGNS
+// (cgsize_t can be long or int depending on how the library was compiled)
+template<typename T>
+op_map cgns_op_decl_map(op_set from, op_set to, int dim, vector<T> *imap, char *name);
+
+template<>
+op_map cgns_op_decl_map<int>(op_set from, op_set to, int dim, vector<int> *imap, char *name) {
+  return op_decl_map(from, to, dim, imap->data(), name);
+}
+
+template<>
+op_map cgns_op_decl_map<long>(op_set from, op_set to, int dim, vector<long> *imap, char *name) {
+  vector<int> new_imap;
+  transform(imap->begin(), imap->end(), new_imap.begin(), [](long x) { return (int)x;});
+  return op_decl_map(from, to, dim, new_imap.data(), name);
+}
 
 int main(int argc, char **argv) {
   // Initialise OP2
@@ -22,7 +42,7 @@ int main(int argc, char **argv) {
 
   int baseIndex = 1;
   int zoneIndex = 1;
-  int numNodes;
+  cgsize_t numNodes;
   char zoneName[33];
   // Get zone name and size
   cg_zone_read(file, baseIndex, zoneIndex, zoneName, &numNodes);
@@ -32,8 +52,8 @@ int main(int argc, char **argv) {
   // Get vertices
   vector<double> x(numNodes);
   vector<double> y(numNodes);
-  int minVertex = 1;
-  int maxVertex = numNodes;
+  cgsize_t minVertex = 1;
+  cgsize_t maxVertex = numNodes;
   cg_coord_read(file, baseIndex, zoneIndex, "CoordinateX",
                 CGNS_ENUMV(RealDouble), &minVertex, &maxVertex, x.data());
   cg_coord_read(file, baseIndex, zoneIndex, "CoordinateY",
@@ -53,7 +73,8 @@ int main(int argc, char **argv) {
   // Get cell section
   char sectionName[33];
   CGNS_ENUMT(ElementType_t) elementType;
-  int elementStart, elementEnd, elementNumBoundary, parentFlag;
+  cgsize_t elementStart, elementEnd;
+  int elementNumBoundary, parentFlag;
   cg_section_read(file, baseIndex, zoneIndex, 1, sectionName, &elementType,
                   &elementStart, &elementEnd, &elementNumBoundary, &parentFlag);
   cout << "Section 1: " << sectionName << endl;
@@ -64,8 +85,8 @@ int main(int argc, char **argv) {
 
   // Get cells
   int numCells = elementEnd - elementStart + 1;
-  vector<int> cgnsCells(numCells * 3);
-  int parentData;
+  vector<cgsize_t> cgnsCells(numCells * 3);
+  cgsize_t parentData;
   cg_elements_read(file, baseIndex, zoneIndex, 1, cgnsCells.data(),
                    &parentData);
 
@@ -76,8 +97,8 @@ int main(int argc, char **argv) {
   op_set cells = op_decl_set(numCells, "cells");
 
   // Declare OP2 maps
-  op_map cell2nodes = op_decl_map(cells, nodes, 3, cgnsCells.data(),
-                                  "cell2nodes");
+  op_map cell2nodes = cgns_op_decl_map<cgsize_t>(cells, nodes, 3, &cgnsCells,
+                                                 "cell2nodes");
 
   // Declare OP2 datasets
   op_dat nodeCoordinates = op_decl_dat(nodes, 2, "double", coords.data(),
