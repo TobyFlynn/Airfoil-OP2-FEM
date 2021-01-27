9935dabdee0b34b8d0fb5127bb41a5b6bce69548
Toby Flynn <toby_flynn@btinternet.com>
2021-01-27 13:27:37 +0000

    Slight changes to mat mult streams

--- src/airfoil_cuda_internal_fluxes.cu
+++ src/airfoil_cuda_internal_fluxes.cu
@@ -18,7 +18,6 @@ void internal_fluxes_matrices(cublasHandle_t handle, const int numCells,
     cudaStreamCreate(&streams[i]);
   }
   for(int c = 0; c < numCells; c++) {
-    cublasSetStream(handle, streams[c % NUMBER_OF_MATRIX_STREAMS]);
     const double *F = F_d + c * 4 * 15;
     const double *G = G_d + c * 4 * 15;
     double *dFdr = dFdr_d + c * 4 * 15;
@@ -26,12 +25,24 @@ void internal_fluxes_matrices(cublasHandle_t handle, const int numCells,
     double *dGdr = dGdr_d + c * 4 * 15;
     double *dGds = dGds_d + c * 4 * 15;
 
+    cublasSetStream(handle, streams[(2*c) % NUMBER_OF_MATRIX_STREAMS]);
     for(int i = 0; i < 4; i++) {
       double alpha = 1.0;
       double beta = 0.0;
       // CUBLAS_OP_T because cublas is column major but constants are stored row major
       cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, F + i, 4, &beta, dFdr + i, 4);
       cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, F + i, 4, &beta, dFds + i, 4);
+      // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, G + i, 4, &beta, dGdr + i, 4);
+      // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, G + i, 4, &beta, dGds + i, 4);
+    }
+
+    cublasSetStream(handle, streams[(2*c + 1) % NUMBER_OF_MATRIX_STREAMS]);
+    for(int i = 0; i < 4; i++) {
+      double alpha = 1.0;
+      double beta = 0.0;
+      // CUBLAS_OP_T because cublas is column major but constants are stored row major
+      // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, F + i, 4, &beta, dFdr + i, 4);
+      // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, F + i, 4, &beta, dFds + i, 4);
       cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, G + i, 4, &beta, dGdr + i, 4);
       cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, G + i, 4, &beta, dGds + i, 4);
     }
