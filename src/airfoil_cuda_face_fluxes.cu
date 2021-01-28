f5f95a6735fe3e62ca79cd4ddb5273d1d3844f53
Toby Flynn <toby_flynn@btinternet.com>
2021-01-28 23:14:13 +0000

    Big performance boost (1 big matrix matrix mult instead of multiple matrix vector mults)

--- src/airfoil_cuda_face_fluxes.cu
+++ src/airfoil_cuda_face_fluxes.cu
@@ -8,41 +8,15 @@ void face_fluxes_matrices(cublasHandle_t handle, const int numCells,
   double *LIFT_d;
   cudaMalloc((void**)&LIFT_d, 15 * 15 * sizeof(double));
   cudaMemcpy(LIFT_d, LIFT, 15 * 15 * sizeof(double), cudaMemcpyHostToDevice);
+
+  double alpha = -1.0;
+  double beta = 1.0;
+
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, LIFT_d, 15, flux0_d, 15, &beta, qRHS0_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, LIFT_d, 15, flux1_d, 15, &beta, qRHS1_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, LIFT_d, 15, flux2_d, 15, &beta, qRHS2_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, LIFT_d, 15, flux3_d, 15, &beta, qRHS3_d, 15);
+
   cudaDeviceSynchronize();
-  // cudaStream_t streams[NUMBER_OF_MATRIX_STREAMS];
-  // for(int i = 0; i < NUMBER_OF_MATRIX_STREAMS; i++) {
-  //   cudaStreamCreate(&streams[i]);
-  // }
-  for(int c = 0; c < numCells; c++) {
-    // cublasSetStream(handle, streams[c % NUMBER_OF_MATRIX_STREAMS]);
-    const double *flux0 = flux0_d + c * 3 * 5;
-    const double *flux1 = flux1_d + c * 3 * 5;
-    const double *flux2 = flux2_d + c * 3 * 5;
-    const double *flux3 = flux3_d + c * 3 * 5;
-    double *qRHS0 = qRHS0_d + c * 15;
-    double *qRHS1 = qRHS1_d + c * 15;
-    double *qRHS2 = qRHS2_d + c * 15;
-    double *qRHS3 = qRHS3_d + c * 15;
-
-    double alpha = -1.0;
-    double beta = 1.0;
-
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, LIFT_d, 15, flux0, 1, &beta, qRHS0, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, LIFT_d, 15, flux1, 1, &beta, qRHS1, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, LIFT_d, 15, flux2, 1, &beta, qRHS2, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, LIFT_d, 15, flux3, 1, &beta, qRHS3, 1);
-
-    // for(int i = 0; i < 4; i++) {
-    //   double alpha = -1.0;
-    //   double beta = 1.0;
-    //   cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, LIFT_d, 15, flux + i, 4, &beta, qRHS + i, 4);
-    // }
-  }
-
-  // for(int i = 0; i < NUMBER_OF_MATRIX_STREAMS; i++) {
-  //   cudaStreamDestroy(streams[i]);
-  // }
-
   cudaFree(LIFT_d);
-  cudaDeviceSynchronize();
 }
--- src/airfoil_cuda_internal_fluxes.cu
+++ src/airfoil_cuda_internal_fluxes.cu
@@ -19,88 +19,29 @@ void internal_fluxes_matrices(cublasHandle_t handle, const int numCells,
   cudaMalloc((void**)&Dsw_d, 15 * 15 * sizeof(double));
   cudaMemcpy(Dsw_d, Dsw, 15 * 15 * sizeof(double), cudaMemcpyHostToDevice);
 
+  double alpha = 1.0;
+  double beta = 0.0;
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, f0_d, 15, &beta, dFdr0_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, f1_d, 15, &beta, dFdr1_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, f2_d, 15, &beta, dFdr2_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, f3_d, 15, &beta, dFdr3_d, 15);
+
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, g0_d, 15, &beta, dGdr0_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, g1_d, 15, &beta, dGdr1_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, g2_d, 15, &beta, dGdr2_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Drw_d, 15, g3_d, 15, &beta, dGdr3_d, 15);
+
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, f0_d, 15, &beta, dFds0_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, f1_d, 15, &beta, dFds1_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, f2_d, 15, &beta, dFds2_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, f3_d, 15, &beta, dFds3_d, 15);
+
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, g0_d, 15, &beta, dGds0_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, g1_d, 15, &beta, dGds1_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, g2_d, 15, &beta, dGds2_d, 15);
+  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 15, numCells, 15, &alpha, Dsw_d, 15, g3_d, 15, &beta, dGds3_d, 15);
+
   cudaDeviceSynchronize();
-  // cudaStream_t streams[NUMBER_OF_MATRIX_STREAMS];
-  // for(int i = 0; i < NUMBER_OF_MATRIX_STREAMS; i++) {
-  //   cudaStreamCreate(&streams[i]);
-  // }
-  for(int c = 0; c < numCells; c++) {
-    const double *f0 = f0_d + c * 15;
-    const double *f1 = f1_d + c * 15;
-    const double *f2 = f2_d + c * 15;
-    const double *f3 = f3_d + c * 15;
-    const double *g0 = g0_d + c * 15;
-    const double *g1 = g1_d + c * 15;
-    const double *g2 = g2_d + c * 15;
-    const double *g3 = g3_d + c * 15;
-    double *dFdr0 = dFdr0_d + c * 15;
-    double *dFdr1 = dFdr1_d + c * 15;
-    double *dFdr2 = dFdr2_d + c * 15;
-    double *dFdr3 = dFdr3_d + c * 15;
-    double *dFds0 = dFds0_d + c * 15;
-    double *dFds1 = dFds1_d + c * 15;
-    double *dFds2 = dFds2_d + c * 15;
-    double *dFds3 = dFds3_d + c * 15;
-    double *dGdr0 = dGdr0_d + c * 15;
-    double *dGdr1 = dGdr1_d + c * 15;
-    double *dGdr2 = dGdr2_d + c * 15;
-    double *dGdr3 = dGdr3_d + c * 15;
-    double *dGds0 = dGds0_d + c * 15;
-    double *dGds1 = dGds1_d + c * 15;
-    double *dGds2 = dGds2_d + c * 15;
-    double *dGds3 = dGds3_d + c * 15;
-
-    double alpha = 1.0;
-    double beta = 0.0;
-
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, f0, 1, &beta, dFdr0, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, f1, 1, &beta, dFdr1, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, f2, 1, &beta, dFdr2, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, f3, 1, &beta, dFdr3, 1);
-
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, f0, 1, &beta, dFds0, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, f1, 1, &beta, dFds1, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, f2, 1, &beta, dFds2, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, f3, 1, &beta, dFds3, 1);
-
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, g0, 1, &beta, dGdr0, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, g1, 1, &beta, dGdr1, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, g2, 1, &beta, dGdr2, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, g3, 1, &beta, dGdr3, 1);
-
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, g0, 1, &beta, dGds0, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, g1, 1, &beta, dGds1, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, g2, 1, &beta, dGds2, 1);
-    cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, g3, 1, &beta, dGds3, 1);
-
-    // cublasSetStream(handle, streams[(2*c) % NUMBER_OF_MATRIX_STREAMS]);
-    // for(int i = 0; i < 4; i++) {
-    //   double alpha = 1.0;
-    //   double beta = 0.0;
-    //   // CUBLAS_OP_T because cublas is column major but constants are stored row major
-    //   cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, F + i, 4, &beta, dFdr + i, 4);
-    //   cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, F + i, 4, &beta, dFds + i, 4);
-    //   // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, G + i, 4, &beta, dGdr + i, 4);
-    //   // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, G + i, 4, &beta, dGds + i, 4);
-    // }
-    //
-    // cublasSetStream(handle, streams[(2*c + 1) % NUMBER_OF_MATRIX_STREAMS]);
-    // for(int i = 0; i < 4; i++) {
-    //   double alpha = 1.0;
-    //   double beta = 0.0;
-    //   // CUBLAS_OP_T because cublas is column major but constants are stored row major
-    //   // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, F + i, 4, &beta, dFdr + i, 4);
-    //   // cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, F + i, 4, &beta, dFds + i, 4);
-    //   cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Drw_d, 15, G + i, 4, &beta, dGdr + i, 4);
-    //   cublasDgemv(handle, CUBLAS_OP_T, 15, 15, &alpha, Dsw_d, 15, G + i, 4, &beta, dGds + i, 4);
-    // }
-  }
-
-  // for(int i = 0; i < NUMBER_OF_MATRIX_STREAMS; i++) {
-  //   cudaStreamDestroy(streams[i]);
-  // }
-
   cudaFree(Drw_d);
   cudaFree(Dsw_d);
-  cudaDeviceSynchronize();
 }
